387a6c2fb4863752b13ad08907b8a091c22528a8
peshemn <peshemn@gmail.com>
2023-10-28 00:56:04 -0300

    Commit inicial.

---
+++ hard.h
@@ -0,0 +1,14 @@
+#ifndef __HARD_H__
+
+#define MOT_DER_A   9
+#define MOT_DER_B   12
+#define MOT_IZQ_A   13
+#define MOT_IZQ_B   14
+
+#define PIN_LED     3
+#define SERVO_DER   6
+#define SERVO_IZQ   7
+
+
+#define __HARD_H__
+#endif
