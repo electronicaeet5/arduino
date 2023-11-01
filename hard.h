84938ec063844608030eddf29c2edb9186f66624
peshemn <peshemn@gmail.com>
2023-11-01 11:00:16 -0300

    Agregado de mas pines

--- hard.h
+++ hard.h
@@ -6,9 +6,13 @@
 #define MOT_IZQ_A   13
 #define MOT_IZQ_B   14
 
-#define PIN_LED     3
+#define PULSADOR    2
+#define LED         3
+#define TRIG        4
+#define ECHO        5
 #define SERVO_DER   6
 #define SERVO_IZQ   7
+#define BUZZER      10
 
 
 #define __HARD_H__
